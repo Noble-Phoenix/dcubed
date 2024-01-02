b76990b49ab3b6c17ceec03b72fcbdf4ffc70c0b
Chris Gillis <cgillis93@gmail.com>
2024-01-02 13:16:26 -0500

    Update cookie policy bannner

--- src/components/common/CookieConsent.js
+++ src/components/common/CookieConsent.js
@@ -18,17 +18,17 @@ const CookieConsent = (props) => {
   }
 
   return (
-    <div className="fixed inset-0 bg-slate-700 bg-opacity-70">
-      <div className="fixed bottom-0 left-0 right-0 flex items-center justify-between px-4 py-8 bg-gray-100">
-        <span className="text-dark text-base mr-16">
+    <div className="fixed inset-0 bg-base-300 ">
+      <div className="fixed bottom-0 left-0 right-0 flex items-center justify-between px-4 py-8 bg-slate-100">
+        <span className="text-black mr-16">
           This website uses cookies to improve user experience. By using our website you consent to all cookies in accordance with our Cookie Policy.
         </span>
-        <button className="btn-secondary py-2 px-8 rounded text-white" onClick={() => acceptCookie()}>
+        <button className="bg-secondary py-2 px-8 rounded text-accent" onClick={() => acceptCookie()}>
           Accept
         </button>
       </div>
     </div>
-  );
+  );  
 };
 
 export default CookieConsent;
